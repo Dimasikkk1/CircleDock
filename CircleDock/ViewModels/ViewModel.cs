48db886b5fdbe5a77a9e692c0597aaae91534a61
Dimasikkk <mr.dimasikkk@mail.ru>
2020-08-19 23:34:15 +1000

    Этот тип не юзается, возможно выпилю его

--- CircleDock/ViewModels/ViewModel.cs
+++ CircleDock/ViewModels/ViewModel.cs
@@ -3,6 +3,7 @@ using System.Runtime.CompilerServices;
 
 namespace CircleDock.ViewModels
 {
+    // юзлес код возможно удалю его
     abstract class ViewModel : INotifyPropertyChanged
     {
         protected virtual void NotifyView([CallerMemberName] string property = "") => PropertyChanged?.Invoke(null, new PropertyChangedEventArgs(property));
