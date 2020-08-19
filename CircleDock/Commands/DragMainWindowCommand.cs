019f9fdcc531baea4e8dc6f25ecd0493e610235e
Dimasikkk <mr.dimasikkk@mail.ru>
2020-08-20 02:00:18 +1000

    Добавил перетаскивание окна

---
+++ CircleDock/Commands/DragMainWindowCommand.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace CircleDock.Commands
+{
+    class DragMainWindowCommand : DelegateCommand<MouseButtonEventArgs>
+    {
+        public DragMainWindowCommand() : base(e =>
+        {
+            if (e.ChangedButton != MouseButton.Left)
+                return;
+
+            ((App)Application.Current).MainWindow.DragMove();
+        })
+        { }
+    }
+}
