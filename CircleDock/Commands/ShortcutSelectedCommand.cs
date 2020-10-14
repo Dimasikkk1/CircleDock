9baaed6a1b6e812bc4f4927d7ec0f3db57cdd0f7
Dimasikkk <mr.dimasikkk@mail.ru>
2020-10-14 23:08:22 +1000

    Добавил запуск программ с ярлыков

---
+++ CircleDock/Commands/ShortcutSelectedCommand.cs
@@ -0,0 +1,15 @@
+﻿using CircleDock.Extensions;
+using CircleDock.Models;
+using System.Windows.Controls;
+
+namespace CircleDock.Commands
+{
+    class ShortcutSelectedCommand : DelegateCommand<SelectionChangedEventArgs>
+    {
+        public ShortcutSelectedCommand() : base((e) =>
+        {
+            ((Shortcut)e.AddedItems[0]).Execute();
+        })
+        { }
+    }
+}
